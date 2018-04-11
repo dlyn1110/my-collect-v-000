118e334d0f72f654996377d3e28205c9c949f63d
Dani Lyn <dlyn1110@gmail.com>
2018-04-11 15:08:34 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+end
+ new_collection
+end
 
 
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection <<yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
